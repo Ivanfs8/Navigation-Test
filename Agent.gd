29108d2fd630c7d8ddb3440c79d6a3109a106ddf
Ivanfs8 <buhosuarez@gmail.com>
2021-07-15 01:10:26 -0300

    test

--- Agent.gd
+++ Agent.gd
@@ -1,35 +1,50 @@
-extends Spatial
+extends KinematicBody
 class_name Agent
 
 export (NodePath) onready var nav = get_node(nav)
-export (NodePath) onready var tween_rot = get_node(tween_rot) as TweenRotation
-export var move_speed: float = 15
-export var rot_speed: float = 280
+export (NodePath) onready var debug_path = get_node(debug_path) as ImmediateGeometry
 
-var path: PoolVector3Array
-var path_ind: int
+export var speed_max := 450.0
+export var acceleration_max := 50.0
+export var angular_speed_max := 240
+export var angular_acceleration_max := 40
 
-onready var agent := GSAISteeringAgent.new()
-#onready var follow_path := GSAIFollowPath.new(agent)
-onready var priority := GSAIPriority.new(agent)
+onready var acceleration := GSAITargetAcceleration.new()
+
+onready var agent := GSAIKinematicBody3DAgent.new(self)
+
+onready var path: GSAIPath = GSAIPath.new([global_transform.origin, global_transform.origin], true)
+onready var follow_path = GSAIFollowPath.new(agent, path)
+
+var valid: bool = false
+
+func _ready():
+	agent.linear_speed_max = speed_max
+	agent.linear_acceleration_max = acceleration_max
+	agent.angular_speed_max = deg2rad(angular_speed_max)
+	agent.angular_acceleration_max = deg2rad(angular_acceleration_max)
+	agent.bounding_radius = 0.8
+	
+	follow_path.deceleration_radius = 1
+	follow_path.arrival_tolerance = 10
+	follow_path.prediction_time = 0.3
+	follow_path.path_offset = 0
+	
+func _physics_process(delta: float):
+	if !valid: return
+	follow_path.calculate_steering(acceleration)
+	
+	agent.movement_type = agent.MovementType.POSITION
+	agent._apply_steering(acceleration, delta)
 
 func move_to(target: Vector3):
 	var dict: Dictionary = nav.find_path(global_transform.origin, target)
-	path = dict["points"]
-	path_ind = 0
 	
-	tween_rot.tween_look_at_deg(get_node("Mesh"), path[1], 135)
-
-func _physics_process(delta: float):
-	if path_ind < path.size():
-		var current_point: Vector3 = path[path_ind]
-		var move_vec: Vector3 = (path[path_ind] - global_transform.origin).normalized()
-		
-		var velocity: Vector3 = move_vec * delta * move_speed
-		
-		translate(velocity)
-		
-		if global_transform.origin.distance_to(current_point) < 0.2:
-			path_ind += 1
-			if path_ind < path.size(): 
-				tween_rot.tween_look_at_deg(get_node("Mesh"), path[path_ind], rot_speed)
+	debug_path.clear()
+	debug_path.begin(Mesh.PRIMITIVE_LINES)
+	for p in dict["points"]:
+		debug_path.add_vertex(p)
+	debug_path.end()
+	
+	path.create_path(dict["points"])
+	valid = true
